78722dc087b9d32c85df90770f78936cbf986397
Mevan Samaratunga <mevan.samaratunga@fmr.com>
2014-08-08 10:22:23 -0400

    Initial Commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'hypervisor'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures hypervisor'
+long_description 'Installs/Configures hypervisor'
+version          '0.1.0'
+
